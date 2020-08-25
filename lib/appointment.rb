a2e2a2714a54fa4dc1e41ca9c6e56effbd9944d8
Leslie G <lesliegonzalez@me.com>
2020-08-25 00:42:24 +0000

    Automatically backed up by Learn

---
+++ lib/appointment.rb
@@ -0,0 +1,20 @@
+class Appointment
+  
+  attr_accessor :date
+  
+  @@all = []
+  
+  def initialize(date, patient, doctor)
+    Appointment.new(date = [], patient, doctor)
+    @@all << self
+  end
+    
+  
+  
+  
+  def self.all
+    @@all
+  end
+  
+  
+end
